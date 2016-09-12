6bfce22619c9aac0be31a9808de33b58940aaae8
Andrii Marynets <andrii.marynets@gmail.com>
2016-09-13 00:33:23 +0300

    problew with template

--- app/views.py
+++ app/views.py
@@ -1,8 +1,11 @@
 from app import app
+from flask import render_template
 
-@app.route("/")
-def hello():
-    return "Hello, world!"
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'nickname': 'Miguel'}  # fake user
+    return render_template('index.html',
+                           title='Home',
+                           user=user)
 
-if __name__ == "__main__":
-    app.run()
