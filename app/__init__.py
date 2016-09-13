632113dd49266f90bf8be7b4e898a773726b4a15
Andrii Marynets <andrii.marynets@gmail.com>
2016-09-13 09:26:56 +0300

    add database

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,14 @@
 from flask import Flask
+from flask.ext.mysql import MySQL
 
-app = Flask("__name__")
-
+app = Flask(__name__)
 from app import views
+
+mysql = MySQL()
+
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'andrew05'
+app.config['MYSQL_DATABASE_DB'] = 'notebookDB'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
--- app/views.py
+++ app/views.py
@@ -1,11 +1,22 @@
-from app import app
 from flask import render_template
+from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'nickname': 'Miguel'}  # fake user
-    return render_template('index.html',
-                           title='Home',
-                           user=user)
-
+    user = { 'nickname': 'Andrii' }
+    posts = [
+        {
+            'author' : {'nickname' : 'John'},
+            'body' : 'Title one'
+        },
+        {
+            'author' : {'nickname' : 'Mike'},
+            'body' : 'Big step in life'
+        }
+    ]
+    return render_template("index.html",
+        title = 'Home',
+        posts = posts,
+        user = user)
