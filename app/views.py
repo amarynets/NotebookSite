48f82a8bc11f30b8b84c1700835c131e4a47315f
Andrii Marynets <andrii.marynets@gmail.com>
2016-09-13 10:23:43 +0300

    output info about notebook

--- app/views.py
+++ app/views.py
@@ -1,10 +1,17 @@
 from flask import render_template
 from app import app
+from flask.ext.mysql import MySQL
 
+mysql = MySQL()
+mysql.init_app(app)
+conn = mysql.connect()
+cursor = conn.cursor()
 
 @app.route('/')
 @app.route('/index')
 def index():
+    cursor.execute("SELECT * FROM Notebook")
+    data = cursor.fetchall()[0:30]
     user = { 'nickname': 'Andrii' }
     posts = [
         {
@@ -19,4 +26,5 @@ def index():
     return render_template("index.html",
         title = 'Home',
         posts = posts,
-        user = user)
+        user = user,
+        data = data)
